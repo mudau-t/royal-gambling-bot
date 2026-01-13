53566456ca4a4c80f0136004e2beea1f8ddea453
mudau-t <mulu20111104@gmail.com>
2026-01-13 11:29:35 +0200

    Update balance.js

--- src/commands/econ/balance.js
+++ src/commands/econ/balance.js
@@ -1,28 +1,13 @@
+const { getUser } = require("../../handlers/userHandler");
+
 module.exports = {
-  name: "help",
-  aliases: ["menu", "commands"],
-  category: "General",
-  description: "Show bot command menu",
+  name: "balance",
+  aliases: ["bal", "coins"],
+  category: "Economy",
+  description: "Check your coin balance",
 
   async execute(message) {
-    const prefix = process.env.PREFIX || "!";
-    const categories = {};
-
-    // Group commands by category
-    message.client.commands.forEach(cmd => {
-      const cat = cmd.category || "Other";
-      if (!categories[cat]) categories[cat] = [];
-      categories[cat].push(cmd.name);
-    });
-
-    let menu = `🤖 **Bot Menu**\n\n`;
-
-    for (const [category, cmds] of Object.entries(categories)) {
-      menu += `📂 **${category}**\n`;
-      menu += cmds.map(c => `\`${prefix}${c}\``).join(", ");
-      menu += "\n\n";
-    }
-
-    message.reply(menu);
+    const user = getUser(message.author.id);
+    message.reply(`💰 You have **${user.balance} coins**`);
   }
 };
