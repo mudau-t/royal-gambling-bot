f1fff06dbc1b74d3e3b910b8e00ea355b238ef39
mudau-t <mulu20111104@gmail.com>
2026-01-13 11:27:25 +0200

    Create menu.js

---
+++ src/commands/econ/general/menu.js
@@ -0,0 +1,28 @@
+module.exports = {
+  name: "menu",
+  aliases: ["help", "commands"],
+  category: "General",
+  description: "Show bot command menu",
+
+  async execute(message) {
+    const prefix = process.env.PREFIX || "!";
+    const categories = {};
+
+    // Group commands by category
+    message.client.commands.forEach(cmd => {
+      const cat = cmd.category || "Other";
+      if (!categories[cat]) categories[cat] = [];
+      categories[cat].push(cmd.name);
+    });
+
+    let menu = `🤖 **Bot Menu**\n\n`;
+
+    for (const [category, cmds] of Object.entries(categories)) {
+      menu += `📂 **${category}**\n`;
+      menu += cmds.map(c => `\`${prefix}${c}\``).join(", ");
+      menu += "\n\n";
+    }
+
+    message.reply(menu);
+  }
+};
